49a889c0cf83cbb24dda8461bf3251eecf11f2f5
lovikamboj <159193447+lovikamboj@users.noreply.github.com>
2024-02-11 21:20:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('hugBtn').addEventListener('click', function() {
+  var hugImg = document.getElementById('hugImg');
+  hugImg.innerHTML = '<img src="https://media.giphy.com/media/l0ExhBpyIOEfCVfwY/giphy.gif">';
+  hugImg.style.display = 'block';
+});
